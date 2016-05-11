0684285f81696f60dea86da7332aa85f0c24ba07
June Shaw <june.shaw@me.com>
2016-05-11 12:40:00 -0600

    types

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,27 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world"); 
+		int myNumber = 78;
+		short myShort = 4;
+		long myLong = 90000000;
+		
+		double myDouble = 3.14324;
+		float myFloat = 3.14f;
+		
+		char myChar = 'J';
+		boolean myBool = true;
+		
+		byte myByte = 127;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBool);
+		System.out.println(myByte);
+		
 	}
 
 }
